c7ed3bb4a3e794fc0dc6ab942454ba77fa93da12
Angel Figuera <angeleraser@gmail.com>
2026-06-24 17:51:15 -0400

    feat: resolve reverse sentence

---
+++ reverseSentence.js
@@ -0,0 +1,18 @@
+/*
+
+Given a string of words, return a new string with the words in reverse order. 
+For example, the first word should be at the end of the returned string, 
+and the last word should be at the beginning of the returned string.
+
+- In the given string, words can be separated by one or more spaces.
+- The returned string should only have one space between words.
+
+*/
+
+function reverseSentence(sentence) {
+  if (typeof sentence !== "string" || !sentence.trim()) {
+    return "";
+  }
+  const words = sentence.trim().split(/\s+/g);
+  return words.reverse().join(" ");
+}
